468a9f1558f0b75d5477e44558114aabee86ff9a
Arielle Leon <ariellel@alleninstitute.org>
2023-08-25 10:08:42 -0700

    updated registration_utils.py

--- code/registration_utils.py
+++ code/registration_utils.py
@@ -822,42 +822,7 @@ def get_frame_rate_platform_json(input_dir: str) -> float:
         return frame_rate
     except IndexError as exc:
         raise Exception(f"Error: {exc}")
-
-def create_input_json(input_file: str, output_dir: str, plane: str) -> dict:
-    """Generates suite2p motion correction input json for a given plane
-
-    Args:
-        input_file (str): path to data directory or path to raw movie
-        output_dir (str): path to results directory
-        plane (int, optional): Plane to process if session directory is passed. Defaults to None.
-
-    Returns:
-        dict: suite2p motion correction input_file json
-    """
-    output_dir = make_output_directory(output_dir, h5_file, plane)
-
-    frame_rate_hz = get_frame_rate_platform_json(input_file)
-    return {
-        "h5py": input_file,
-        "movie_frame_rate_hz": frame_rate_hz,
-        "motion_corrected_output": os.path.join(output_dir, f"suite2p_motion_output.h5"),
-        "motion_diagnostics_output": os.path.join(
-            output_dir, f"suite2p_rigid_motion_transform.csv"
-        ),
-        "max_projection_output": os.path.join(
-            output_dir, f"suite2p_maximum_projection.png"
-        ),
-        "avg_projection_output": os.path.join(
-            output_dir, f"suite2p_average_projection.png"
-        ),
-        "registration_summary_output": os.path.join(
-            output_dir, f"suite2p_registration_summary.png"
-        ),
-        "motion_correction_preview_output": os.path.join(
-            output_dir, f"suite2p_motion_preview.webm"
-        ),
-        "output_json": os.path.join(output_dir, f"suite2p_motion_correction_output.json"),
-    }
+    
 
 def write_output_metadata(metadata: dict, raw_movie: Union[str, Path], motion_corrected_movie: Union[str, Path]) -> None:
     """Writes output metadata to plane processing.json
@@ -904,17 +869,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     plane = args.plane
+    h5_file = args.input_filename
     if plane is None:
-        h5_file = glob(f"{args.input_dir}/*.h5")[0]
-        output_dir = args.output_dir
+        h5_file = glob(f"{h5_file}/*.h5")[0]
     else:
         h5_file = glob(
-            f"{args.input_dir}/Other_[0-9]*/Other/ophys/planes/{plane}/*um.h5"
+            f"{h5_file}/Other_[0-9]*/Other/ophys/planes/{plane}/*um.h5"
         )[0]
-        output_dir = os.path.join(args.output_dir, plane)
-        os.makedirs(output_dir, exist_ok=True)
+    output_dir = make_output_directory(args.output_dir, h5_file, plane)
     try:
-        frame_rate_hz = get_frame_rate_platform_json(args.input_dir)
+        frame_rate_hz = get_frame_rate_platform_json(h5_file)
     except Exception:
         frame_rate_hz = 30.
     data = {"h5py": h5_file, "movie_frame_rate_hz": frame_rate_hz}
