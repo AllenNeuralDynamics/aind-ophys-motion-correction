590b22c3f39304b68e801eabf81fb15be00547ce
Jerome Lecoq <jeromel@alleninstitute.org>
2023-09-11 22:40:13 +0000

    Edited registration.py

--- code/registration.py
+++ code/registration.py
@@ -1238,7 +1238,8 @@ if __name__ == "__main__":  # pragma: nocover
 
     print(args)
     # We construct the paths to the outputs
-    args["movie_frame_rate_hz"] = frame_rate_hz
+    args.movie_frame_rate_hz = frame_rate_hz
+    
     for key, default in (
         ("motion_corrected_output", "_registered.h5"),
         ("motion_diagnostics_output", "_motion_transform.csv"),
