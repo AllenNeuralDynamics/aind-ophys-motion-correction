d0a7a7b8d31463c47731ad58da1825aec92e2679
Arielle Leon <ariellel@alleninstitute.org>
2023-09-12 21:31:21 +0000

    Edited registration_utils.py

--- code/registration_utils.py
+++ code/registration_utils.py
@@ -901,7 +901,6 @@ if __name__ == "__main__":
         else:
             plane = None
     data_description = find_file(data_dir, "data_description.json")
-    print(f"~~~~~~~~~~~~~~~~{data_description}")
     with open(os.path.join(output_dir, "data_description_path.log"), "w") as f:
         f.writelines(f"{data_description}")
     with open(data_description) as f:
