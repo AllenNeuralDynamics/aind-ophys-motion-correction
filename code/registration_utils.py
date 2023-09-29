d43e5f9f670071daaf664cb801d544f062ea9d78
Arielle Leon <ariellel@alleninstitute.org>
2023-09-29 16:54:23 +0000

    Edited registration_utils.py

--- code/registration_utils.py
+++ code/registration_utils.py
@@ -871,8 +871,6 @@ if __name__ == "__main__":
     else:
         exp_id = str(experiment_folders[0]).split("_")[-1]
         h5_file = find_file(str(data_dir), f"{exp_id}.h5")
-    with open(output_dir / 'log.txt', "w") as f:
-        f.writelines(str(data_dir))
     print(f"DATA DIR {data_dir}")
     
     experiment_id = h5_file.name.split(".")[0]
