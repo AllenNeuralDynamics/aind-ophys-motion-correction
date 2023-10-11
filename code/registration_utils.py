dfc1de4e0b7d907179abb838307ee2ac1a71c4f6
Arielle Leon <ariellel@alleninstitute.org>
2023-10-11 23:40:56 +0000

    Edited registration_utils.py

--- code/registration_utils.py
+++ code/registration_utils.py
@@ -902,6 +902,7 @@ if __name__ == "__main__":
         data = json.load(f)
     if not experiment_folders:
         sync_file = [i for i in list(data_dir.glob(data['sync_file']))][0]
+        print(list(data_dir.glob("*/*.h5")))
         h5_file = [i for i in list(data_dir.glob("*/*.h5"))][0]
         experiment_id = h5_file.name.split(".")[0]
     else:
