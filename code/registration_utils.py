f14f434a969e9dba12727ffe4a1927a523bc5d89
Arielle Leon <ariellel@alleninstitute.org>
2023-10-12 11:58:45 -0700

    removing prints

--- code/registration_utils.py
+++ code/registration_utils.py
@@ -915,7 +915,6 @@ if __name__ == "__main__":
         data = json.load(f)
     if not experiment_folders:
         file_list = list(data_dir.glob("*.h5"))
-        print(f"~~~~~~~~~~~~~~FILE LIST: {file_list}")
         file_list.remove(data["sync_file"])
         sync_file = [i for i in list(data_dir.glob(data['sync_file']))][0]
         h5_file = [i for i in file_list if re.findall("\d{9}.h5", str(i))][0]
