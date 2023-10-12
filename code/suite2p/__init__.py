2a4ac655b2a5f7322b2f130cd5d91d229cb8f467
Arielle Leon <ariellel@alleninstitute.org>
2023-10-12 12:27:50 -0700

    vendoring s2p for debugging

---
+++ code/suite2p/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copright © 2023 Howard Hughes Medical Institute, Authored by Carsen Stringer and Marius Pachitariu.
+"""
+from .version import version
+from .default_ops import default_ops
+from .run_s2p import run_s2p, run_plane, pipeline
+from .detection import ROI, detection_wrapper
+from .classification import classify
+from .extraction import extraction_wrapper
+from .registration import registration_wrapper
+
+name = "suite2p"
