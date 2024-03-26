1e014afbaa13e9350bd194a2034820d03f58e904
Arielle Leon <ariellel@alleninstitute.org>
2024-03-26 18:01:45 +0000

    Edited registration.py

--- code/registration.py
+++ code/registration.py
@@ -1410,6 +1410,7 @@ if __name__ == "__main__":  # pragma: nocover
     datainput = Path(args.input_searchpath)
     output_dir = Path(args.output_dir)
     data_dir = Path("../data")
+    import pdb;pdb.set_trace()
     try:
         data_description = next(data_dir.glob("*/data_description.json"))
     except:
