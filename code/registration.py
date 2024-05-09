53eca81c8dfabdae26ce6dd3af1c4c2e86246656
Arielle Leon <ariellel@alleninstitute.org>
2024-05-09 03:02:24 +0000

    Edited registration.py

--- code/registration.py
+++ code/registration.py
@@ -1185,7 +1185,10 @@ def multiplane_motion_correction(datainput: Path, output_dir: Path, debug: bool 
             experiment_id = [i for i in datainput.glob("*") if i.is_dir()][
             0
             ].name
-        h5_file = [i for i in datainput.glob("*/*") if f"{experiment_id}.h5" in str(i)][0]
+        try:
+            sync_file = [i for i in session_dir.glob(platform_data["sync_file"])][0]
+        except IndexError:
+            sync_file = next(datainput.glob(platform_data["sync_file"]))
     session_dir = h5_file.parent.parent
     platform_json = next(session_dir.glob("*platform.json"))
     # this file is required for paired plane registration but not for single plane
