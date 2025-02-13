1f56f03b4b077f37457fb35d76857b2c8ac65fa2
arielleleon <ariellel@alleninstitute.org>
2025-02-13 21:05:24 +0000

    Edited registration.py

--- code/registration.py
+++ code/registration.py
@@ -1611,12 +1611,13 @@ def multiplane_motion_correction(data_dir: Path, output_dir: Path, debug: bool =
     """
     pattern = re.compile(r"^V.*\d+$")
     test = [d for d in data_dir.rglob("*.txt")]
-    print(f"TEST>>>>>>>>>>>>>>>>>>>{test}")
     matching_files = [
         d for d in data_dir.rglob("*.txt") if pattern.match(d.stem)
     ]
     if len(matching_files) > 0:
-        h5_file = next(data_dir.rglob(matching_files[0]))
+        with open(matching_files[0], "r") as f:
+            h5_file = f.read()
+        h5_file = next(data_dir.rglob(h5_file))
         unique_id = h5_file.stem
     else:
         h5_dir = [i for i in data_dir.rglob("*VI*") if i.is_dir()][0]
